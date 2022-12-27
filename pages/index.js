7f774f0510c2444584539b0a40a87e3bd954e9c0
soga-johnny <soga.johnny@gmail.com>
2022-12-27 20:46:31 +0900

    scrollanimation

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,32 @@
 import Head from 'next/head'
 // import Image from 'next/image'
-import { Canvas, useThree } from '@react-three/fiber'
-import { Image, Scroll, ScrollControls } from "@react-three/drei"
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Image, Scroll, ScrollControls, useScroll } from "@react-three/drei"
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
-// import { useRef } from 'react'
+import { useRef } from 'react'
 
 
 
 function Images() {
 
   const { height } = useThree((state) => state.viewport);
+  const ref = useRef();
+  const deta = useScroll();
+
+  useFrame(()=>{
+    ref.current.children[0].material.zoom = 1 +  deta.range(0, 1 / 3 ) / 3
+  })
 
  return (
-    <group>
-      <Image url = "./img1.png" alt = "drill" scale = {[2.6, 1.8, 1]} position = {[0, 0.5, 1]}/>
+    <group ref = {ref}>
+      <Image url = "./img1.png" alt = "drill" scale = {[2.6, 1.8, 1]} position = {[0, 0.5, 1]} />
       <Image url = "./img2.png" alt = "drill" scale = {[2.2, 2.8, 2]} position = {[-0.3, height -12, 2]}/>
-      <Image url = "./img3.png" alt = "drill" scale = {[3, 2.2, 3]} position = {[0, -height -0.8, 1]}/>
-      <Image url = "./img4.png" alt = "drill" scale = {[1.6, 2, 4]} position = {[0, -height -2.2, 2]}/>
-      <Image url = "./img5.png" alt = "drill" scale = {[2, 1.5, 5]} position = {[0.4, -height -3.8, 1.5]}/>
-      <Image url = "./img6.png" alt = "drill" scale = {[2.8, 2, 6]} position = {[-0.1, -height -5.5, 1]}/>
-      <Image url = "./img7.png" alt = "drill" scale = {[1, 0.8, 6]} position = {[-0.4, -height -6.6, 2]}/>
+      <Image url = "./img3.png" alt = "drill" scale = {[3, 2.2, 3]} position = {[0, -height -0.8, 1]} transparent opacity={0.8}/>
+      <Image url = "./img4.png" alt = "drill" scale = {[1.6, 2, 4]} position = {[0, -height -2.2, 2]}  transparent opacity={0.8}/>
+      <Image url = "./img5.png" alt = "drill" scale = {[2, 1.5, 5]} position = {[0.4, -height -3.8, 1.5]}  transparent opacity={0.8}/>
+      <Image url = "./img6.png" alt = "drill" scale = {[2.8, 2, 6]} position = {[-0.1, -height -5.5, 1]}  transparent opacity={0.8}/>
+      <Image url = "./img7.png" alt = "drill" scale = {[1, 0.8, 6]} position = {[-0.4, -height -6.6, 2]}  transparent opacity={0.8}/>
     </group>
   );
 }
@@ -47,38 +53,59 @@ export default function Home() {
       <main className={styles.main}>
         <section className={styles.canvas}>
           <Canvas>
-            <ScrollControls pages={3.2} damping={5}>
+            <ScrollControls pages={3.5} damping={5}>
+
               <Scroll>
             <Images />
             </Scroll>
-            <Scroll html>
+
+           <Scroll html className={styles.htmls}>
               <h2>
               <span className={styles.fvCopy}>
-                  What name <br/>
-                  is this?
+                  Whats the name <br/>
+                  this work?
               </span>
               </h2>
+
+              <span className={styles.scroll}></span>
+
+              <h2>
+              <span className={styles.sec2}>
+                   Be immediately <br/>
+                   identified
+              </span>
+              </h2>
+
+              <h2>
+              <span className={styles.sec3}>
+                  Storage of<br/>
+                 4,000+ items
+              </span>
+              </h2>
+
+             <footer className={styles.footer}>
+              <p>
+              this is documentsite <br/>
+              that scanning <br/>
+              pain-tart to don’t know<br/>
+              it names ever.
+              </p>
+              <div className={styles.footerLogo}>
+                <Link href="/">
+                  <h2>A-galleria</h2>
+                </Link>
+              </div>
+             </footer>
+
             </Scroll>
+            
             </ScrollControls>
           </Canvas>
         </section>
 
       </main>
 
-      <footer className={styles.footer}>
-        <p>
-        this is documentsite <br/>
-        that scanning <br/>
-        pain-tart to don’t know<br/>
-        it names ever.
-        </p>
-
-        <div className={styles.footerLogo}>
-          <Link href="/">
-            <h2>A-galleria</h2>
-          </Link>
-        </div>
-      </footer>
+      
     </div>
   )
 }
