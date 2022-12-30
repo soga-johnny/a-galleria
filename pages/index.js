3f934ae105db5e0bd4ab9df0e1667ec657245009
soga-johnny <soga.johnny@gmail.com>
2022-12-30 18:07:29 +0900

    unchange-scanJs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-// import Image from 'next/image'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Image, Scroll, ScrollControls, useScroll } from "@react-three/drei"
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
 import { useRef } from 'react'
+import { motion } from "framer-motion";
 
 
 function Images() {
@@ -15,17 +15,18 @@ function Images() {
 
   useFrame(()=>{
     ref.current.children[0].material.zoom = 1 +  deta.range(0, 1 / 3 ) / 3
+    ref.current.children[1].material.zoom = 1 +  deta.range(0, 1 / 3 ) / 4
   })
 
  return (
     <group ref = {ref}>
       <Image url = "./img1.png" alt = "drill" scale = {[2.6, 1.8, 1]} position = {[0, 0.5, 1]} />
       <Image url = "./img2.png" alt = "drill" scale = {[2.2, 2.8, 2]} position = {[-0.3, height -12, 2]}/>
-      <Image url = "./img3.png" alt = "drill" scale = {[3, 2.2, 3]} position = {[0, -height -0.8, 1]} transparent opacity={0.7}/>
-      <Image url = "./img4.png" alt = "drill" scale = {[1.6, 2, 4]} position = {[0, -height -2.2, 2]}  transparent opacity={0.7}/>
-      <Image url = "./img5.png" alt = "drill" scale = {[2, 1.5, 5]} position = {[0.4, -height -3.8, 1.5]}  transparent opacity={0.7}/>
-      <Image url = "./img6.png" alt = "drill" scale = {[2.8, 2, 6]} position = {[-0.1, -height -5.5, 1]}  transparent opacity={0.7}/>
-      <Image url = "./img7.png" alt = "drill" scale = {[1, 0.8, 6]} position = {[-0.4, -height -6.6, 2]}  transparent opacity={0.7}/>
+      <Image url = "./img3.png" alt = "drill" scale = {[3, 2.2, 3]} position = {[0, -height -0.8, 1]} transparent opacity={0.8}/>
+      <Image url = "./img4.png" alt = "drill" scale = {[1.6, 2, 4]} position = {[0, -height -2.2, 2]}  transparent opacity={0.8}/>
+      <Image url = "./img5.png" alt = "drill" scale = {[2, 1.5, 5]} position = {[0.4, -height -3.8, 1.5]}  transparent opacity={0.8}/>
+      <Image url = "./img6.png" alt = "drill" scale = {[2.8, 2, 6]} position = {[-0.1, -height -5.5, 1]}  transparent opacity={0.8}/>
+      <Image url = "./img7.png" alt = "drill" scale = {[1, 0.8, 6]} position = {[-0.4, -height -6.6, 2]}  transparent opacity={0.8}/>
     </group>
   );
 }
@@ -40,17 +41,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={styles.header}>
+      <motion.header className={styles.header}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 50 }}
+        transition={{  duration: 1, delay: 1, ease: "easeInOut" }}
+        >
         <h1 className={styles.logoname}>
           A-galleria
         </h1>
          <button className={styles.scan}>
+          <Link href="/scan">
             Scan
+            </Link>
             </button>
-      </header>
+      </motion.header>
 
       <main className={styles.main}>
-        <section className={styles.canvas}>
+        <motion.section className={styles.canvas}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{  duration: 1.5, ease: "easeInOut" }}
+          viewport={{ once: true }}
+          >
           <Canvas>
             <ScrollControls pages={3.22} damping={5}>
 
@@ -59,8 +71,8 @@ export default function Home() {
             </Scroll>
 
            <Scroll html className={styles.htmls}>
-              <h2>
-              <span className={styles.fvCopy}>
+              <h2 className={styles.fvCopy}>
+              <span>
                   Whats the name <br/>
                   this work?
               </span>
@@ -68,15 +80,15 @@ export default function Home() {
 
               <span className={styles.scroll}></span>
 
-              <h2>
-              <span className={styles.sec2}>
+              <h2 className={styles.sec2}>
+              <span>
                    Be immediately <br/>
                    identified
               </span>
               </h2>
 
-              <h2>
-              <span className={styles.sec3}>
+              <h2 className={styles.sec3}>
+              <span>
                   Storage of<br/>
                  4,000+ items
               </span>
@@ -88,7 +100,9 @@ export default function Home() {
               Give it a try.
               </p>
               <button className={styles.footerScan}>
-              Scan
+              <Link href="/scan">
+            Scan
+            </Link>
               </button>
               <div className={styles.footerLogo}>
                 <Link href="/">
@@ -101,7 +115,7 @@ export default function Home() {
             
             </ScrollControls>
           </Canvas>
-        </section>
+        </motion.section>
 
       </main>
 
---
+++ pages/scan.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { motion } from "framer-motion";
+import styles from '../styles/Home.module.scss'
+
+export default function Scan() {
+return (
+    <div className={styles.container}>
+        <div className={styles.comingSoon}>
+            <p>
+                Preparing AR contents...
+            </p>
+        </div>
+
+    </div>
+)
+}
